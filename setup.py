4d58953d1707d8f68efeb210600be15df37b97b2
Joe Dallago <jd.dallago@gmail.com>
2011-12-30 18:35:35 -0600

    Initial config files, and setuptools files.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#RadiantlyDire/setup.py
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README')).read()
+CHANGES = open(os.path.join(here, 'CHANGES')).read()
+
+entry_points = """
+      [paste.app_factory]
+      main = radiantlydire:main
+      """
+
+requires = ['pyramid',
+            'pyramid_debugtoolbar',
+            'sqlalchemy',
+            'psycopg2',
+            'alembic',
+            'pyramid_beaker']
+
+setup(name='RadiantlyDire',
+      version='0.1dev',
+      description='',
+      long_description=README + '\n\n' + CHANGES,
+      install_requires=requires,
+      url='http://localhost',
+      packages=['radiantlydire'],
+      test_suite='radiantlydire.tests',
+      entry_points = entry_points
+)
